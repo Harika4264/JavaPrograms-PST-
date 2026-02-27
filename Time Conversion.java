ef41565c7eafb1a75d5ee78b8025c25eebfab3ba
Harika4264 <harikagp786@gmail.com>
2026-02-27 09:32:46 +0530

    Add files via upload

---
+++ Time Conversion.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String timeConversion(String s) {
+    String period=s.substring(8);
+    int hour=Integer.parseInt(s.substring(0,2));
+    String rest=s.substring(2,8);
+    if(period.equals("AM")){
+        if(hour==12){
+            hour=0;
+        }
+    } else {
+        if(hour!=12){
+            hour+=12;
+        }
+    }
+    return String.format("%02d%s",hour,rest);
+    }
+
+}
+public class Solution{
+    public static void main(String[] args) throws IOException{
+        BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(System.in));
+        String s=bufferedReader.readLine();
+        String result=Result.timeConversion(s);
+        System.out.println(result);
+        bufferedReader.close();
+    }
+}
+
